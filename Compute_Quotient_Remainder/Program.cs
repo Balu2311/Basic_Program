791367c639e2121488fb0fad3594a0dadc64dd73
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 21:42:55 +0530

    Compute_Quotient_Remainders

---
+++ Compute_Quotient_Remainder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Compute_Quotient_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Dividend : ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor : ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend: " + dividend + " Divisor: " + divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
