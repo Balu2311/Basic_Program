de048c38173939803d75f9062573eb326cc7d3e6
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 22:22:17 +0530

    check Vowel Consonant

---
+++ VowelConsonant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VowelConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Alphabet is Vowel or Consonant");
+            Console.WriteLine(" Enter a character : ");
+            char ch = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') //|| ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine(ch + " is a Vowel ");
+            }
+            else if ((ch >= 'A' && ch <= 'Z')) // || (ch >= 'a' && ch <= 'z'))
+            {
+                Console.WriteLine(ch + " is a Consonant ");
+            }
+            else
+            {
+                Console.WriteLine(ch + " not an Alphabate");
+            }
+        }
+    }
+}
