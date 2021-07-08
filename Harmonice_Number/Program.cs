6cba21bcd452d94230f1c7e8317c8c68202b42c7
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 21:38:04 +0530

    Harmonic_ Number find

---
+++ Harmonice_Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harmonice_Number
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("*** Welcome to Harmoni Nth Number find ***");
+            Console.WriteLine("Enter the Nth value: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Harmonic values:  " + N);
+            int i ;
+            double s = 0.0;
+            if (N <= 0 )
+            {
+                Console.WriteLine(" Please Enter the Positive value ");
+                return;
+            }
+            else
+            {
+                for (i = 1; i <= N; i++)
+                {
+                    Console.Write("1/{0}  ", i);
+                    s = 1 / (float)i;
+                    
+                }
+                Console.Write("\n\n{0}th Harmonic Number : {1} \n", N, s);
+                //Console.Write("\nSum of Series upto {0} terms : {1} \n", N, s); // s += 1 / (float)i;  Sum of the harmonic value
+            }
+
+        }
+    }
+}
