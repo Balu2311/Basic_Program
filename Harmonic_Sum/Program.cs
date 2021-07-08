de899384e1b6c5f689298f55986c9c2af93566d7
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 21:37:07 +0530

    Harmonic _ sum

---
+++ Harmonic_Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Harmonic_Sum
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("*** Welcome to Harmonic Sum ");
+            int i, N;
+            double s = 0.0;
+
+            Console.Write("Enter the Harmonic Number: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            if(N<0)
+            {
+                Console.WriteLine("Please Enter the Positive Value");
+            }
+            for (i = 1; i <= N; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\n\nSum of Series upto {0} terms : {1} \n", N, s);
+        }
+    }
+}
