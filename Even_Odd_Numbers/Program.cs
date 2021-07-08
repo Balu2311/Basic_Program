c803b10ad5e1829bcca88b82c93097253f4e6fb5
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 21:48:33 +0530

    Check Even_Odd_Numbers

---
+++ Even_Odd_Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Even_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Even and Odd check Numbers");
+            Console.WriteLine("Enter a Number : ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            if (numb % 2 == 0)
+                Console.WriteLine(numb + " is Even Number");
+            else
+                Console.WriteLine(numb + " is Odd Number");
+        }
+    }
+}
