80e82e2efe80c807b858624388135a204f41713f
Vallem Balu <vbnreddy5@gmail.com>
2021-07-08 21:49:24 +0530

    Print Even_Odd_Numbers

---
+++ Even_Odd_nu_print/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Even_Odd_nu_print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Even and Odd numbers");
+            Console.WriteLine("Enter the range of number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEven Numbers : ");
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+
+                }
+                
+            }
+            Console.WriteLine("\nOdd Numbers :");
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
